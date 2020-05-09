729b2e5f18518396099101b4d6d0879fe5f05574
Jeyhun <jeyhunmustafazadeh@gmail.com>
2020-05-09 20:28:38 +0400

    rr

--- src/main/java/com/tech/docker/dockerspringboot/resource/HelloResource.java
+++ src/main/java/com/tech/docker/dockerspringboot/resource/HelloResource.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloResource {
     @GetMapping
     public String hello(){
-        return "Hello docker !!!";
+        return "Hello docker and Jeyhun !!!";
     }
 }
